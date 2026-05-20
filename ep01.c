ad67781d7ae5a1fa9901b8938d33c814257d94f8
Michael Queiroz <78761691+mchqueiroz@users.noreply.github.com>
2026-05-20 17:31:11 -0300

    oi7

--- ep01.c
+++ ep01.c
@@ -63,6 +63,95 @@ char buscarArvore(link raiz, const char* codigoMorse){
     return atual->key; // Retorna a letra correspondente ao código Morse
 }
 
+void imprimirPreOrdem(link raiz, char* string, int sizeString){
+    if (raiz == NULL)
+        return;
+
+    if (string == NULL){
+        char* novaString = (char*)malloc(5 * sizeof(char)); // string que guarda o codigo-morse do caractere do no
+        for (int i=0; i < 5; i++){
+            novaString[i] = ' ';
+        }
+        novaString[0] = '.';
+        imprimirPreOrdem(raiz->left, novaString, sizeString + 1);
+    
+        novaString[0] = '-';
+        imprimirPreOrdem(raiz->right, novaString, sizeString + 1);
+
+        free(novaString);
+    } else{
+        for (int i=sizeString; i < 5; i++)
+            string[i] = ' ';
+
+        for(int i=0; i < sizeString - 1; i++){
+            printf("    |");
+        }
+
+        printf("\'");
+        for(int i=0; i < 5; i++){
+            if (i == 4)
+                printf("%c",  string[i]);
+            else
+                printf("%c ", string[i]);
+        }
+        printf("\'");
+        if (raiz->key != '\0')
+            printf(" -> %c\n", raiz->key);
+        else
+            printf("\n");
+        
+        string[sizeString] = '.';
+        imprimirPreOrdem(raiz->left, string, sizeString + 1);
+    
+        string[sizeString] = '-';
+        imprimirPreOrdem(raiz->right, string, sizeString + 1);
+    }
+}
+
+void traduzirLetra(link raiz, char letra, char* string, int sizeString, int* achou){
+    if (raiz == NULL)
+        return;
+    if (achou == NULL){
+        int* boolAchou = (int*)malloc(sizeof(int));
+        *boolAchou = 0;
+        achou = boolAchou;
+    }
+    if (*achou == 1)
+        return;
+
+    if (string == NULL){
+        char* novaString = (char*)malloc(5 * sizeof(char)); // string que guarda o codigo-morse do caractere do no
+        for (int i=0; i < 5; i++){
+            novaString[i] = ' ';
+        }
+        novaString[0] = '.';
+        traduzirLetra(raiz->left, letra, novaString, sizeString + 1, achou);
+    
+        novaString[0] = '-';
+        traduzirLetra(raiz->right, letra, novaString, sizeString + 1, achou);
+
+        free(novaString);
+        free(achou);
+    } else{
+        
+        for (int i=sizeString; i < 5; i++)
+            string[i] = ' ';
+        
+        if (raiz->key == letra){
+            *achou = 1;
+            for (int i=0; i < 5; i++)
+                printf("%c ", string[i]);
+            printf("\n");
+            return;
+        }
+        
+        string[sizeString] = '.';
+        traduzirLetra(raiz->left, letra, string, sizeString + 1, achou);
+    
+        string[sizeString] = '-';
+        traduzirLetra(raiz->right, letra, string, sizeString + 1, achou);
+    }
+}
 
 int main(){
     char linha[256];// Buffer para ler linhas do arquivo
@@ -83,7 +172,7 @@ int main(){
     // Lê o arquivo linha por linha, extrai a letra e o código Morse, e insere na árvore
     while(fgets(linha, sizeof(linha), arquivo)){// Lê cada linha do arquivo
         if (sscanf(linha, " %c %s", &letra, codigo) == 2) {// Extrai a letra e o código Morse da linha
-            printf("Lido: %c -> %s\n", letra, codigo);// Imprime a letra e o código Morse lidos para verificação
+            //printf("Lido: %c -> %s\n", letra, codigo);// Imprime a letra e o código Morse lidos para verificação
             tree = inserirArvore(tree, letra, codigo);// Insere a letra na árvore de acordo com o código Morse
         }
     }
@@ -124,8 +213,8 @@ int main(){
             token = strtok(NULL, " "); // Continua usando estritamente o espaço
         }
         printf("\n");
-
     }
+    
     return 0;
 
 }
